5d8e42291d7033667d1c8e09b6ef0fbf237a9dc7
Jon-117 <38222965+Jon-117@users.noreply.github.com>
2024-04-14 20:20:09 -0500

    Vid 1 and 1/2 of vid 2. (11:55)

--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    app_name = 'VidCollector'
+    return render(request, 'video_collection/home.html', {'app_name':app_name})
