763a471f4a4efa4cd5c8dac574f7215d82cfc711
Jon-117 <38222965+Jon-117@users.noreply.github.com>
2024-04-14 22:04:33 -0500

    added video_list.html page, added minor validation

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,11 +1,27 @@
+from urllib import parse
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 
 class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
-    notes = models.TextField(blank=True, null=True) # blank acceptable, null in db acceptable
+    notes = models.TextField(blank=True, null=True)  # blank acceptable, null in db acceptable
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query
+        if not query_string:
+            raise ValidationError('Invalid YouTube URL')
+        parameters = parse.parse_qs(query_string, strict_parsing=True)
+        v_parameters_list = parameters.get('v')  # return None if not found (looking for '?')
+        if not v_parameters_list:
+            raise ValidationError('Invalid YouTube URL')
+        self.video_id = v_parameters_list[0]
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.name}\nNotes: {self.notes[:200]} '
--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Video
+from .forms import VideoForm
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 
 
 # Create your views here.
@@ -6,3 +11,30 @@ from django.shortcuts import render
 def home(request):
     app_name = 'VidCollector'
     return render(request, 'video_collection/home.html', {'app_name':app_name})
+
+
+def add(request):
+
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            try:
+                new_video_form.save()
+                # messages.info(request, 'Saved successfully!')
+                return redirect(video_list)
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'Vid already collected')
+
+
+        messages.warning(request, 'Invalid, check your input!')
+        return render(request, 'video_collection/add.html', {'form': new_video_form})
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form':new_video_form})
+
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_collection/video_list.html', {'videos':videos})
